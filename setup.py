6959842c5b517dceb43e8661b3a09e4a69256d46
Sampreet Vaidya <93003875+sampreetucalgary07@users.noreply.github.com>
2023-09-11 20:29:23 -0600

    Update setup.py
    Adding dash emulator github link from sampreet's account

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ requirements = [
     "aioquic",
     "matplotlib",
     "pyyaml",
-    "dash-emulator @ git+https://github.com/navidakbari/dash-emulator#egg=dash-emulator"
+    "dash-emulator @ git+https://github.com/sampreetucalgary07/dash-emulator#egg=dash-emulator"
 ]
-
+#https://github.com/navidakbari/dash-emulator#egg=dash-emulator
 setup(name='dash-emulator-quic',
       version='0.2.0.dev0',
       description='A headless player to emulate the playback of MPEG-DASH streams over QUIC',

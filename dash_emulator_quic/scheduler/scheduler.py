567835e1544be3be8b5b073da0521cbc1d354709
sampreetucalgary07 <sampreet.vaidya@ucalgary.ca>
2023-12-27 10:15:21 +0530

    logic True

--- dash_emulator_quic/scheduler/scheduler.py
+++ dash_emulator_quic/scheduler/scheduler.py
@@ -124,7 +124,7 @@ class BETASchedulerImpl(BETAScheduler):
             print("selections before logic : ", self._current_selections)
 
             # Select if you want to implement logic
-            logic = False
+            logic = True
 
             num_previous_samples = 3
             # calculate slope

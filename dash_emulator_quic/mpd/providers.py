063d82609b58f5383bb5e472b8c6711d64b357db
sampreetucalgary07 <sampreet.vaidya@ucalgary.ca>
2024-03-28 17:38:12 -0600

    removed prints

--- dash_emulator_quic/mpd/providers.py
+++ dash_emulator_quic/mpd/providers.py
@@ -40,10 +40,10 @@ class BETAMPDProviderImpl(MPDProvider):
     async def update(self):
         await self.download_manager.download(self.mpd_url, save=True)
         content, size = await self.download_manager.wait_complete(self.mpd_url)
-        print("Dash Emulator Quic")
-        print("COntent : ", content)
-        print("Size : ", size)
-        print("URL : ", self.mpd_url)
+        # print("Dash Emulator Quic")
+        # print("COntent : ", content)
+        # print("Size : ", size)
+        # print("URL : ", self.mpd_url)
 
         text = content.decode("utf-8")
         self._mpd = self.parser.parse(text, url=self.mpd_url)

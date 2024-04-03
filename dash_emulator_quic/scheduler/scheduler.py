a8652f873891b5e7d1c5ed8fe586b692a86b40aa
sampreetucalgary07 <sampreet.vaidya@ucalgary.ca>
2024-04-03 10:41:36 -0600

    L False

--- dash_emulator_quic/scheduler/scheduler.py
+++ dash_emulator_quic/scheduler/scheduler.py
@@ -130,7 +130,7 @@ class BETASchedulerImpl(BETAScheduler):
 
             # print("Selections_before_logic : ", self._current_selections[0])
             # Select if you want to implement logic
-            logic = True
+            logic = False
 
             # Initialize slope, red_value and selected_values
             slope = "NA"

abf3219266e84bb73a89cf87d5bab0d391cf7e10
sampreetucalgary07 <sampreet.vaidya@ucalgary.ca>
2024-07-02 16:01:20 -0600

    def value = 6

--- dash_emulator_quic/scheduler/scheduler.py
+++ dash_emulator_quic/scheduler/scheduler.py
@@ -173,7 +173,7 @@ class BETASchedulerImpl(BETAScheduler):
             self.qual_list.append(self._current_selections[0])
             # n = 0
             # print("Len of Listener : ", len(self.listeners))
-            stall_logic = True
+            stall_logic = False
             # listener_tasks = []
             no_of_stalls_to_consider = 2
             diff_between_stalls = 30
@@ -209,7 +209,7 @@ class BETASchedulerImpl(BETAScheduler):
                         logic,
                     )
                     # await asyncio.gather(*listener_tasks)
-
+            self._current_selections[0] = 6
             duration = 0
             urls = []
             print("Selections : ", self._current_selections)
